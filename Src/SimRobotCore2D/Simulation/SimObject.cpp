a7ae304cec3c96f55524c38615ece7b369a344c2
Shlok Sabarwal <shlok190303@gmail.com>
2024-01-12 17:03:01 +0530

    Updating SimRobot code to allow for custom robot and ball positions

--- Src/SimRobotCore2D/Simulation/SimObject.cpp
+++ Src/SimRobotCore2D/Simulation/SimObject.cpp
@@ -21,49 +21,85 @@ SimObject::~SimObject()
 
 void SimObject::registerObjects()
 {
-  for(SimObject* simObject : children)
+  for (SimObject *simObject : children)
   {
-    if(simObject->name.empty())
+    if (simObject->name.empty())
     {
-      const char* typeName = typeid(*simObject).name();
+      const char *typeName = typeid(*simObject).name();
 #ifdef WINDOWS
-      const char* str = std::strchr(typeName, ' ');
-      if(str)
+      const char *str = std::strchr(typeName, ' ');
+      if (str)
         typeName = str + 1;
 #else
-      while(*typeName >= '0' && *typeName <= '9')
+      while (*typeName >= '0' && *typeName <= '9')
         ++typeName;
 #endif
       simObject->fullName = fullName + "." + typeName;
     }
+
     else
       simObject->fullName = fullName + "." + QString::fromStdString(simObject->name);
-    CoreModule::application->registerObject(*CoreModule::module, dynamic_cast<SimRobot::Object&>(*simObject), dynamic_cast<SimRobot::Object*>(this));
+
+    CoreModule::application->registerObject(*CoreModule::module, dynamic_cast<SimRobot::Object &>(*simObject), dynamic_cast<SimRobot::Object *>(this));
     simObject->registerObjects();
   }
 }
 
-void SimObject::addParent(Element& element)
+void SimObject::registerObjects(float r_x, float r_z, float b_x, float b_z)
 {
-  dynamic_cast<SimObject*>(&element)->children.push_back(this);
+  for (SimObject *simObject : children)
+  {
+    if (simObject->name.empty())
+    {
+      const char *typeName = typeid(*simObject).name();
+#ifdef WINDOWS
+      const char *str = std::strchr(typeName, ' ');
+      if (str)
+        typeName = str + 1;
+#else
+      while (*typeName >= '0' && *typeName <= '9')
+        ++typeName;
+#endif
+      simObject->fullName = fullName + "." + typeName;
+    }
+
+    else
+      simObject->fullName = fullName + "." + QString::fromStdString(simObject->name);
+
+    // Adding custom code to update the robot position to a custom value!
+    if (simObject->name == "robot5")
+      simObject->poseInParent.translate(r_x, 0.0f, r_z);
+
+    // Adding custom code to update the ball position to a custom value!
+    if (simObject->name == "ball")
+      simObject->poseInParent.translate(b_x, 0.0f, b_z);
+
+    CoreModule::application->registerObject(*CoreModule::module, dynamic_cast<SimRobot::Object &>(*simObject), dynamic_cast<SimRobot::Object *>(this));
+    simObject->registerObjects();
+  }
 }
 
-const QString& SimObject::getFullName() const
+void SimObject::addParent(Element &element)
+{
+  dynamic_cast<SimObject *>(&element)->children.push_back(this);
+}
+
+const QString &SimObject::getFullName() const
 {
   return fullName;
 }
 
-const QIcon* SimObject::getIcon() const
+const QIcon *SimObject::getIcon() const
 {
   return &CoreModule::module->objectIcon;
 }
 
-SimRobot::Widget* SimObject::createWidget()
+SimRobot::Widget *SimObject::createWidget()
 {
   return new SimObjectWidget(*this);
 }
 
-SimRobotCore2D::Painter* SimObject::createPainter()
+SimRobotCore2D::Painter *SimObject::createPainter()
 {
   return new SimObjectPainter(*this);
 }
--- Src/SimRobotCore2D/Simulation/SimObject.h
+++ Src/SimRobotCore2D/Simulation/SimObject.h
@@ -29,41 +29,44 @@ public:
   /** Registers child objects in the scene graph. */
   virtual void registerObjects();
 
-  QString fullName; /**< The path name to the object in the scene graph. */
-  std::string name; /**< The name of the object in the scene graph (without the path to it). */
-  std::list<SimObject*> children; /**< The list of children of this object. */
-  b2Vec2* translation = nullptr; /**< The initial translation of this object relative to its parent. */
-  b2Rot* rotation = nullptr; /**< The initial rotation of this object relative to its parent */
-  QTransform transformation; /**< The transformation of this object relative to its parent (or the world if this is a body). */
+  /** Registers child objects in the scene graph. (with custom positions for one robot and ball)*/
+  virtual void registerObjects(float r_x, float r_z, float b_x, float b_z);
+
+  QString fullName;                /**< The path name to the object in the scene graph. */
+  std::string name;                /**< The name of the object in the scene graph (without the path to it). */
+  std::list<SimObject *> children; /**< The list of children of this object. */
+  b2Vec2 *translation = nullptr;   /**< The initial translation of this object relative to its parent. */
+  b2Rot *rotation = nullptr;       /**< The initial rotation of this object relative to its parent */
+  QTransform transformation;       /**< The transformation of this object relative to its parent (or the world if this is a body). */
 
 protected:
   /**
    * Registers another scene graph element as parent of this element.
    * @param element The element to register.
    */
-  void addParent(Element& element) override;
+  void addParent(Element &element) override;
 
   /**
    * Returns the full path to the object in the scene graph.
    * @return The full path ...
    */
-  [[nodiscard]] virtual const QString& getFullName() const;
+  [[nodiscard]] virtual const QString &getFullName() const;
 
   /**
    * Returns an icon to visualize the object in the scene graph.
    * @return An icon ...
    */
-  [[nodiscard]] virtual const QIcon* getIcon() const;
+  [[nodiscard]] virtual const QIcon *getIcon() const;
 
   /**
    * Creates a widget for this object.
    * @return The new widget instance.
    */
-  virtual SimRobot::Widget* createWidget();
+  virtual SimRobot::Widget *createWidget();
 
   /**
    * Creates a painter for this object.
    * @return The new painter instance.
    */
-  virtual SimRobotCore2D::Painter* createPainter();
+  virtual SimRobotCore2D::Painter *createPainter();
 };
